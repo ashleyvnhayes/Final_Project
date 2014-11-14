26af0f8c488faecade4c39e8c6a0420e476a50b6
tonerikohime <ashley.heizu@gmail.com>
2014-11-13 19:53:28 -0800

    Initial set up
    Created table with images, decided first milestone

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var cards = ['img/beach.jpg','img/luna.jpg','img/onsen.jpg','img/potter.jpg','img/ramenjpg','img/red_velvet.jpg','img/venus.jpg','img/winery.jpg'];
+$(document).ready(function(){
+
+
+
+});	
+
+
+
+//first milestone: show the grid, show each cell having an image and having pair image elsewhere. upon reloading show different order 
+
+// assignImagesToCards();
+// -- for each card
+// -- -- if this card has already been assigned, skip this card
+// -- -- pick a random pair card (that hasn't been assigned yet)
+// -- -- pick a random element from the images list
+// -- -- assign the image to both cards
+
+
