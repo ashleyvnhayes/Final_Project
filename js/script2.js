c35422858d59ba6fd60cfd4f1406c5a36f5b596d
tonerikohime <ashley.heizu@gmail.com>
2014-11-17 20:12:42 -0800

    Set up flip orientation in HTML
    Set up cards with front/back. Prepping for flip!

--- js/script2.js
+++ js/script2.js
@@ -1,5 +1,3 @@
-var cards = ['img/beach.jpg','img/luna.jpg','img/onsen.jpg','img/potter.jpg','img/ramen.jpg','img/red_velvet.jpg','img/venus.jpg','img/winery.jpg','img/beach.jpg','img/luna.jpg','img/onsen.jpg','img/potter.jpg','img/ramen.jpg','img/red_velvet.jpg','img/venus.jpg','img/winery.jpg'];
-
 $(document).ready(function(){
 	$('button').on('click', function(){
 		
@@ -7,6 +5,7 @@ $(document).ready(function(){
 	    var pics = board.children();
 	    for (i = 0; i < pics.length; i++){
 		board.append($('td')).append(pics.splice(Math.floor(Math.random() * pics.length), 1)[0]);
+		
     	
 		};
 		
@@ -27,22 +26,3 @@ $(document).ready(function(){
 // -- -- assign the image to both cards
 
 
-// morsels of things that work:
-// $('button').on('click', function(){
-// $('<img src="' + cards[Math.floor(Math.random() * cards.length)] + '">').appendTo('#board')}); pics from array appear with click of button
-
-// for (i = 0; i < table.length; i++){
-// 	$('td').append()
-
-
-
-// 	$('button').on('click', function(){
-// 		$('img').remove();
-// 		for (i = 0; i < cards.length; i++){
-// 			$('<img src="' + cards[Math.floor(Math.random() * cards.length)] + '">').css({"width": "180px", "height": "160px", "float": "left"}).appendTo('tbody');
-// 			$('tbody').css({"max-width": "720px"});
-
-// 		};
-// 	});
-
-// }
